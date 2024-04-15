f2360612674cfb059fa7ba17ae167f658f7cf850
Phobos-Duck <161325120+Phobos-Duck@users.noreply.github.com>
2024-04-15 11:22:12 +0600

    1111

--- bot.py
+++ bot.py
@@ -1,10 +1,12 @@
 import asyncio
 import logging
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher, types, F
 from aiogram.filters.command import Command
+import parser
 
 
 answer_lek = ""
+in_user = {}
 logging.basicConfig(level=logging.INFO)
 bot = Bot(token='6994195142:AAGS4WZuOaYNzc0sOEd4F_cPoWkS8rxqHqg')
 dp = Dispatcher()
@@ -14,10 +16,18 @@ async def cmd_start(message: types.Message):
     await message.answer("Добро пожаловать! Вас приветствует сервис по поиску лекарств в аптеке uteka.ru")
     await message.answer("Чтобы я начал работу, подскажите, что вы ищите?")
 
-@dp.message(content_types=types.ContentType.TEXT)
+@dp.message(F.text)
 async def on_text_message(message: types.Message):
-    await message.reply("Понял! Попробую найти...")
+    answer_lek = message.text
+    await message.reply(f"Понял! Попробую найти {answer_lek}")
+    parser.parse(answer_lek)
+    for key, value in in_user.items():
+        await message.answer(f"Вот например{key} по цене {value}")
 async def main():
     await dp.start_polling(bot)
+
+def filtered(**filtered):
+    for key, value in filtered.items():
+        in_user[key] = value
 def start():
     asyncio.run(main())
--- parser.py
+++ parser.py
@@ -1,21 +1,21 @@
 from bs4 import BeautifulSoup
 import requests
 import random
+import bot
 
-def user_agent():
+def user_agent(word):
     user_agents = ['Mozilla/5.0 (iPhone; CPU iPhone OS 5_1 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9B179 Safari/7534.48.3'
         'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
         'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
         'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
         'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36']
-    word = "Антигриппин"
-    url_new = "https://omsk.uteka.ru/search/?query=Антигриппин"
+    url_new = f"https://omsk.uteka.ru/search/?query={word}"
     HEADER = {'User-Agent': random.choice(user_agents)}
     page = requests.get(url_new, data=HEADER)
     return page
 
-def parse():
-    connect = user_agent()
+def parse(word):
+    connect = user_agent(word)
     filtered = {}
     if connect.status_code == 200:
         soup = BeautifulSoup(connect.text, "html.parser")
@@ -25,6 +25,6 @@ def parse():
                 names = data.find('span', {'itemprop': 'name'}).text
                 buy = data.find('div', {'class': 'ui-price__content'}).text
                 filtered[names] = buy
+                bot.filtered(filtered)
             except AttributeError:
                 return False
-            print(filtered)
